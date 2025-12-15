a3283239d3b74d64fdcf90c0971a759b39fb8fb5
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-12-15 12:18:38 +0530

    'migrated input component too redux'

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import ToDoTask from './ToDoTask'
+import React from "react";
+import ToDoTask from "./ToDoTask";
+import { useSelector } from "react-redux";
 
-function ToDoList({taskData , editTask , deleteTask}) {
-
-  if(taskData.length === 0)
-  {
-    return  <div 
-    className=
-        'mt-12 mx-auto w-2/5 py-4 px-2 bg-zinc-600 flex flex-col justify-center items-center gap-8 rounded-xl'>
-          <p className='text-white font-semibold text-lg pb-0'>OOPs !</p>
-          <p className='text-white text-md font-medium pt-0'>No task added yet!</p>
-        </div>
+function ToDoList({ editTask, deleteTask }) {
+  const tasks = useSelector((state) => state.tasks);
+  if (tasks.length === 0) {
+    return (
+      <div className="mt-12 mx-auto w-2/5 py-4 px-2 bg-zinc-600 flex flex-col justify-center items-center gap-8 rounded-xl">
+        <p className="text-white font-semibold text-lg pb-0">OOPs !</p>
+        <p className="text-white text-md font-medium pt-0">
+          No task added yet!
+        </p>
+      </div>
+    );
   }
 
   return (
-    <div 
-    className=
-        'mt-12 mx-auto w-5/6 py-14 px-2 bg-zinc-600 flex flex-col justify-center items-center gap-8 rounded-xl'>
-          {
-            taskData.map((item , index) => (
-              <ToDoTask key={index} data = {item} editTask ={editTask} deleteTask ={deleteTask} />
-            ))
-          }
+    <div className="mt-12 mx-auto w-5/6 py-14 px-2 bg-zinc-600 flex flex-col justify-center items-center gap-8 rounded-xl">
+      {tasks.map((item, index) => (
+        <ToDoTask
+          key={index}
+          data={item}
+          editTask={editTask}
+          deleteTask={deleteTask}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ToDoList
+export default ToDoList;
