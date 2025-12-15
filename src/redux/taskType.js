8389665e60520e89cab30bdb0f5e993918b0a6f0
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-12-15 11:57:50 +0530

    'created the types of action of tasks'

--- src/redux/taskType.js
+++ src/redux/taskType.js
@@ -1,1 +1,17 @@
 // add , edit , delete
+// Purpose
+// This file defines action type constants.
+
+// Only string constants that describe events:
+// ADD_TASK
+// DELETE_TASK
+// EDIT_TASK
+// TOGGLE_TASK
+
+// Nothing else
+
+
+export const ADD_TASK = 'ADD_TASK'    
+export const DELETE_TASK = 'DELETE_TASK'    
+export const EDIT_TASK = 'EDIT_TASK'    
+
